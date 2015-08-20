b5955c833f60b8fbb4a6b9c6827a237800079981
Gen2sai <PWRChong@hotmail.com>
2015-08-20 17:03:11 +0800

    UserLogin DAL/BLL NLog

---
+++ SportRegistrationSystem/GymnasticRegister/Helper/ErrorLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using System.Diagnostics;
+
+namespace GymnasticRegister.Helper
+{
+    class ErrorLog
+    {
+        private static Logger ExceptionLogger = LogManager.GetLogger("ExceptionLog");
+
+        public static void LogError(Exception ex)
+        {
+            MappedDiagnosticsContext.Set("date", DateTime.Now.ToString());
+            MappedDiagnosticsContext.Set("exception", ex.ToString());
+
+            ExceptionLogger.Error("");
+        }
+    }
+}
