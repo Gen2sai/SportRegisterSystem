8cfb107851c22ea2f34766514b6e14eb2e4a060a
Gen2sai <PWRChong@hotmail.com>
2015-08-26 18:00:03 +0800

    Rename UserDLL/BLL, Added Create and Login

---
+++ SportRegistrationSystem/GymnasticRegister/BusinessLogicLayer/StaffBLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using GymnasticRegister.DataAccessLayer;
+
+namespace GymnasticRegister.BusinessLogicLayer
+{
+    internal class StaffBLL
+    {
+        public bool Login(string Username, string Password)
+        {
+            int loginCount = StaffDAL.LoginStaff(Username, Password);
+
+            return loginCount == 1;
+        }
+
+        public bool RegisterStaff(string Username, string Password, string Fullname, int Permission)
+        {
+            int status = StaffDAL.CreateStaff(Username, Password, Fullname, Permission);
+
+            return status != 0;
+        }
+    }
+}
