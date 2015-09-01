0dabdbaf5a549b4d29049b719aefd8f87a8d55a2
Gen2sai <PWRChong@hotmail.com>
2015-09-01 16:03:53 +0800

    Included Model  for StudentClass

---
+++ SportRegistrationSystem/GymnasticRegister/Model/StudentClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymnasticRegister.Model
+{
+    public class StudentClass
+    {
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int GradeID { get; set; }
+
+        public int Age { get; set; }
+
+        public int ContactNumber { get; set; }
+    }
+}
