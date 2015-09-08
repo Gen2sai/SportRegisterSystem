3a4376447e62996538510ed912cd7bb2944306d6
Gen2sai <PWRChong@hotmail.com>
2015-09-08 10:05:57 +0800

    CreateStudent method, added DAL and BLL for student

---
+++ SportRegistrationSystem/GymnasticRegister/BusinessLogicLayer/StudentBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymnasticRegister.DataAccessLayer;
+
+namespace GymnasticRegister.BusinessLogicLayer
+{
+    internal class StudentBLL
+    {
+        public static bool RegisterStudent(string studentName, int grade, int age, int contactNumber, string username)
+        {
+            int status = StudentDAL.CreateStudent(studentName, grade, age, contactNumber, username);
+            return status == 1;
+        }
+    }
+}
---
+++ SportRegistrationSystem/GymnasticRegister/DataAccessLayer/StudentDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+using GymnasticRegister.Helper;
+
+namespace GymnasticRegister.DataAccessLayer
+{
+    class StudentDAL
+    {
+        public static int CreateStudent(string studentName, int grade, int age, int contactNumber, string username)
+        {
+            try
+            {
+                int status;
+                SqlConnection conn = new SqlConnection(ConnectionConfig.GetConnectionString());
+                SqlCommand cmd;
+
+                cmd =
+                new SqlCommand(
+                    "IF(NOT EXISTS(SELECT StudentID FROM Student WHERE StudentName = @studentName AND GradeID = @grade AND Age = @age)) BEGIN INSERT INTO STUDENT (StudentName, GradeID, Age, ContactNumber, CreatedBy) VALUES (@studentName, @grade, @age, @contactNumber, @username) END",
+                    conn);
+                cmd.Parameters.AddWithValue("@studentName", studentName);
+                cmd.Parameters.AddWithValue("@grade", grade);
+                cmd.Parameters.AddWithValue("@age", age);
+                cmd.Parameters.AddWithValue("@contactNumber", contactNumber);
+                cmd.Parameters.AddWithValue("@username", username);
+
+                conn.Open();
+                status = cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                ErrorLog.LogError(ex);
+                return 0;
+            }
+        }
+    }
+}
