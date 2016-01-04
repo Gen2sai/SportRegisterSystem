950deed4a85a93cb9d74b93c5701195cf64fdc42
sqlite3 --version <Genryu@gmail.com>
2016-01-04 12:04:59 +0800

    DB Backup for replication, ready for deployment.

---
+++ SportRegistrationSystem/GymnasticRegister/Forms/QuoteManagementForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GymnasticRegister.Forms
+{
+    partial class QuoteManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblAddQuote = new System.Windows.Forms.Label();
+            this.lblQuoteList = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.lblAddQuote);
+            this.groupBox1.Controls.Add(this.lblQuoteList);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(545, 455);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Quote Management Form";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(464, 423);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "button2";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(383, 423);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "button1";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(81, 372);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(458, 45);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // lblAddQuote
+            // 
+            this.lblAddQuote.AutoSize = true;
+            this.lblAddQuote.Location = new System.Drawing.Point(10, 372);
+            this.lblAddQuote.Name = "lblAddQuote";
+            this.lblAddQuote.Size = new System.Drawing.Size(35, 13);
+            this.lblAddQuote.TabIndex = 2;
+            this.lblAddQuote.Text = "label1";
+            // 
+            // lblQuoteList
+            // 
+            this.lblQuoteList.AutoSize = true;
+            this.lblQuoteList.Location = new System.Drawing.Point(7, 20);
+            this.lblQuoteList.Name = "lblQuoteList";
+            this.lblQuoteList.Size = new System.Drawing.Size(35, 13);
+            this.lblQuoteList.TabIndex = 1;
+            this.lblQuoteList.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(533, 329);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // QuoteManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 479);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "QuoteManagementForm";
+            this.Text = "QuoteManagementForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblQuoteList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblAddQuote;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ SportRegistrationSystem/GymnasticRegister/Forms/QuoteManagementForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GymnasticRegister.BusinessLogicLayer;
+using GymnasticRegister.Resources;
+
+
+namespace GymnasticRegister.Forms
+{
+    public partial class QuoteManagementForm : Form
+    {
+        private int refStaffId;
+        private int refpermission;
+
+        public QuoteManagementForm(int StaffId, int Permission)
+        {
+            refStaffId = StaffId;
+            refpermission = Permission;
+            InitializeComponent();
+            DataTable dt = QuoteBLL.LoadQuotes();
+            dataGridView1.DataSource = dt;
+            lblAddQuote.Text = SportRegistrationSystem.lblAddQuote;
+            lblQuoteList.Text = SportRegistrationSystem.lblQuoteList;
+            btnCancel.Text = SportRegistrationSystem.lblCancel;
+            btnSubmit.Text = SportRegistrationSystem.lblSubmit;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            StaffMenuForm form = new StaffMenuForm(refStaffId, refpermission);
+            form.Show();
+            this.Close();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string Quote = richTextBox1.Text;
+            if(!string.IsNullOrEmpty(Quote))
+            {
+                QuoteBLL.AddQuotes(Quote);
+                DataTable dt = QuoteBLL.LoadQuotes();
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+            }
+        }
+    }
+}
