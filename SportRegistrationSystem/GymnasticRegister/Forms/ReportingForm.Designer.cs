2e07eb033be4b7b5887a78bd7997b2fd6f649e7d
sqlite3 --version <Genryu@gmail.com>
2016-02-04 09:10:40 +0800

    Reporting form which was not commited last commit.

---
+++ SportRegistrationSystem/GymnasticRegister/Forms/ReportingForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace GymnasticRegister.Forms
+{
+    partial class ReportingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnLookup = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 19);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(114, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeydownHandler_KeyDown);
+            // 
+            // btnLookup
+            // 
+            this.btnLookup.Location = new System.Drawing.Point(126, 16);
+            this.btnLookup.Name = "btnLookup";
+            this.btnLookup.Size = new System.Drawing.Size(75, 23);
+            this.btnLookup.TabIndex = 0;
+            this.btnLookup.Text = "button1";
+            this.btnLookup.UseVisualStyleBackColor = true;
+            this.btnLookup.Click += new System.EventHandler(this.btnLookup_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.btnBack);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.btnLookup);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(901, 447);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ReportingForm";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(889, 367);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnBack
+            // 
+            this.btnBack.AutoEllipsis = true;
+            this.btnBack.Location = new System.Drawing.Point(826, 418);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "button2";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // ReportingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 471);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ReportingForm";
+            this.Text = "ReportingForm";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnLookup;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ SportRegistrationSystem/GymnasticRegister/Forms/ReportingForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GymnasticRegister.BusinessLogicLayer;
+using GymnasticRegister.Resources;
+
+namespace GymnasticRegister.Forms
+{
+    public partial class ReportingForm : Form
+    {
+        int tempStaffId;
+        int tempPermission;
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        } 
+
+        public ReportingForm(int StaffId, int permission)
+        {
+            InitializeComponent();
+            btnLookup.Text = SportRegistrationSystem.lblLookup;
+            btnBack.Text = SportRegistrationSystem.lblBack;
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "MMM/yyyy";
+            tempStaffId = StaffId;
+            tempPermission = permission;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            MainMenuForm form = new MainMenuForm(tempStaffId, tempPermission);
+            form.Show();
+            this.Close();
+        }
+
+        private void btnLookup_Click(object sender, EventArgs e)
+        {
+            //DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            DateTime startDate = new DateTime();
+            string tempDate = dateTimePicker1.Value.ToString("MMM-yyyy");
+            startDate = DateTime.Parse(tempDate);
+            DataTable dt = ReportingBLL.ReportLookup(startDate);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void KeydownHandler_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnLookup_Click(this, new EventArgs());
+            }
+        }
+    }
+}
